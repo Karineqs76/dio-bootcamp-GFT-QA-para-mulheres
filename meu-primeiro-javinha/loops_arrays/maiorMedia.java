6e4f70381b5df682c96d4364f730e25076ab997a
Karineqs76 <karineqs0706@hotmail.com>
2024-06-25 21:48:36 -0300

    commit media

---
+++ meu-primeiro-javinha/loops_arrays/maiorMedia.java
@@ -0,0 +1,28 @@
+package loops_arrays;
+
+import java.util.Scanner;
+
+public class maiorMedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+        int soma = 0;
+        int count = 0;
+
+        do {
+            System.out.println("Numero: ");
+            numero = scan.nextInt();
+
+            soma = soma + numero;
+
+            if (numero > maior) maior = numero;
+
+            count ++;
+        }
+        while(count < 5);
+        System.out.println("Maior: " + maior);
+        System.out.println("Média: " + (soma/5));
+    }
+}
