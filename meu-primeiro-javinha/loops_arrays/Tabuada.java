fdfec3da5049bbe8eb708fcedb67a2a91854d1cd
Karineqs76 <karineqs0706@hotmail.com>
2024-07-06 21:26:57 -0300

    commit tabuada

---
+++ meu-primeiro-javinha/loops_arrays/Tabuada.java
@@ -0,0 +1,17 @@
+package loops_arrays;
+import java.util.Scanner;
+
+public class Tabuada {
+public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+
+     System.out.println("Escolha um numero: ");
+     int tabuada = scan.nextInt();
+
+     System.out.println("Tabuada de: " + tabuada);
+     for(int i = 1; i <= 10; i ++){
+        System.out.println(tabuada + "X" + i + "=" + (tabuada*i));
+     }
+
+}
+}
